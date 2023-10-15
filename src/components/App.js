74a835d9a326fdcda21aba7642bba112a74eebc4
hkpbadnawar <60421293+hari371@users.noreply.github.com>
2023-10-15 11:27:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const[height,setHeight] = useState(300);
   return (
     <div id="main">
       <img src={star}  height={`${height}+px`} width={`${height}+px`} onClick{()=>{
